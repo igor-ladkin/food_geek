44589ff2e1de3c6b4eb2a4e75b3a98286baa4961
Igor Ladkin <dgkplan@googlemail.com>
2020-05-29 21:12:23 +0200

    Add integration test for showing published recipes

---
+++ test/food_geek_web/integration/show_published_recipe.exs
@@ -0,0 +1,24 @@
+defmodule FoodGeekWeb.SignUpTest do
+  use FoodGeekWeb.IntegrationCase
+
+  setup do
+    chef = FoodGeek.Accounts.get_user_by(email: "jiro@sushi.com")
+    published_at = DateTime.utc_now() |> DateTime.truncate(:second)
+    sushi = insert!(:recipe, title: "Maguro Nigiri", chef: chef, published_at: published_at)
+
+    [sushi: sushi]
+  end
+
+  @tag logged_as: "seth@pe.com"
+  test "User can go to home page and click on recipe card to see published recipe", %{
+    conn: conn,
+    sushi: sushi
+  } do
+    get(conn, "/")
+    |> follow_link(Routes.recipe_path(conn, :show, sushi))
+    |> assert_response(
+      status: 200,
+      html: "Jiro Ono"
+    )
+  end
+end
